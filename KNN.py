5cc39b35c59d883c324274e0cc52b6b68a7f19f4
Halfdisposal <bikashjyotisaharia1065@gmail.com>
2025-04-20 13:14:29 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,68 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random
+from sklearn.datasets import load_digits
+import math
+import heapq 
+from sklearn.model_selection import train_test_split
+
+data = load_digits()
+X, y = data.data, data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 class KNN:
-    def __init__(self, n_neighbors = 5):
-        self.k = n_neighbors
-        self.pred_labels = None
-    
-    def calcDist(self, point, points):
-        dists = []
-        for p in points:
-            dists.append(((point[0] - p[0])**2 + (point[1] - p[1])**2)**0.5)
-        return dists 
-    def findFirstMin(self, dists):
-        nearestn = []
-        nearest_labels = []
-        for i in range(dists.shape[0]):
-            nearest_for_i = []
-            i_label = []
-            for j in range(min(self.k, self.n_points) + 1):
-                dist_arr_for_i = dists[i]
-                min_index = np.argmin(dist_arr_for_i)
-                if min_index != i:
-                    nearest_for_i.append(min_index.item())
-                    i_label.append(self.y_train[min_index].item())
-                dist_arr_for_i[min_index] = np.inf
-            
-            nearestn.append(nearest_for_i)
-            nearest_labels.append(i_label)
-            
-        return nearestn, nearest_labels
-
-
-    def fit(self, X, y):
-        self.X_train = X
-        self.y_train = y
-        
-        self.n_points = X.shape[0]
-        dists = []
-        for i in range(self.n_points):
-            dists.append(self.calcDist(X[i], X))
-        dists = np.array(dists)
-        nearest_neighbors, labels = self.findFirstMin(dists)
-        final_labels = []
-        for i in range(len(labels)):
-            label_count = {}
-            for label in labels[i]:
-                if label in label_count:
-                    label_count[label] += 1
+    def __init__(self, X, y, k = 3):
+        self.X = X
+        self.y = y 
+        self.k = k
+        self.X_test = []
+        self.y_pred = []
+        self.y_test = []
+    def findDist(self, point):
+        dist = []
+        for p, l in zip(self.X, self.y):
+            d = 0
+            for ptrain, ptest in zip(p, point):
+                d += (ptrain - ptest)**2
+            d = d**0.5
+            dist.append((d.item(), l.item()))
+        return dist
+    def findLabel(self, point):
+        dists = self.findDist(point)
+        min_dists = heapq.nsmallest(self.k, dists, key=lambda x: x[0])
+        labels = [x[1] for x in min_dists]
+        return min_dists, labels
+    def predict(self, X_test, y_test = None):
+        self.X_test = X_test
+        self.y_test = y_test
+        for point in X_test:
+            dists, labels = self.findLabel(point)
+            counts = {}
+            for l in labels:
+                if l in counts:
+                    counts[l] += 1
                 else:
-                    label_count[label] = 1
-            final_labels.append(max(label_count, key=label_count.get))
-            self.pred_labels = final_labels
-        return final_labels
-
-    def confusionMat(self):
-        confusion_matrix = np.zeros((2, 2))
-        for i in range(len(self.pred_labels)):
-            confusion_matrix[self.y_train[i]][self.pred_labels[i]] += 1
-        return confusion_matrix
-    
+                    counts[l] = 1
+            
+            label = sorted(counts, key = counts.get)[0]
+            self.y_pred.append(label)
+        return self.y_pred
     def score(self):
-        correct = 0
-        for i in range(len(self.pred_labels)):
-            if self.pred_labels[i] == self.y_train[i]:
-                correct += 1
-        return correct / len(self.pred_labels)
+        scores = list(self.y_pred == self.y_test)
+        counts = scores.count(np.True_)
+        return counts/len(scores)
+    def confusionMat(self):
+        num_classes = len(np.unique(self.y_test))  # Determine the number of classes
+        confusion_matrix = np.zeros((num_classes, num_classes), dtype=int)  # Initialize matrix
+        for i in range(len(self.y_pred)):
+            confusion_matrix[self.y_test[i]][self.y_pred[i]] += 1
+        return confusion_matrix
+
+
+
+
+
+m ,n = 4, 2
+knn = KNN(X_train, y_train, 4)
+labels = knn.predict(X_test, y_test)
+print(knn.score())
+print(knn.confusionMat())
+
+
